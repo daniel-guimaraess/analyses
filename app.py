98d7fc72c5458cca702fe98b332f039cad180fc0
Daniel Guimarães <daniel_guimaraes2012@hotmail.com>
2025-03-28 09:43:19 -0300

    Melhorias

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ def group_by_user(dataframe):
     
     grouped = dataframe.groupby('Nome').agg(
         Datas=('Data', lambda x: x.dt.strftime('%d/%m/%Y').unique().tolist()),
-        Dias=('Data', lambda x: x.dt.date.nunique())
+        **{'Total dias': ('Data', lambda x: x.dt.date.nunique())}
     ).reset_index()
     
     return grouped
 
-st.title('Altave análise')
-st.text('Faça upload do arquivo CSV, para uma análise detalhada dos acessos e presenças dos colaboradores.')
+st.title('Altave Facial')
+st.text('Análise detalhada dos acessos e presenças dos colaboradores.')
 
 st.sidebar.image("https://altave.com.br/wp-content/webp-express/webp-images/uploads/2021/11/Novo_logo_ALTAVE-1024x683.png.webp", width=300)
 
@@ -38,11 +38,8 @@ def toggle_sort():
     st.session_state.sort_ascending = not st.session_state.sort_ascending
 
 if uploaded_file is not None:
-
     dataframe = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
-
     dataframe_clean = clean_csv(dataframe)
-    
     data = group_by_user(dataframe_clean)
     
     options = ['Todos'] + data['Nome'].tolist()
@@ -60,20 +57,19 @@ if uploaded_file is not None:
     
     text = "Análise Geral" if selected_user == 'Todos' else f'Análise detalhada do usuário: {selected_user}'
     st.subheader(text)
-    st.dataframe(filtered_data, use_container_width=True)
+    st.dataframe(filtered_data, use_container_width=True, hide_index=True)
     
     if selected_user == 'Todos':
-
         st.write(f"Quantidade de dias por pessoa:")
         st.button("Ordenar gráfico pela quantidade de dias", on_click=toggle_sort)
         
-        sort_order = alt.SortField(field='Dias', order='ascending' if st.session_state.sort_ascending else 'descending')
+        sort_order = alt.SortField(field='Total dias', order='ascending' if st.session_state.sort_ascending else 'descending')
         num_users = len(filtered_data)
         height_graph = max(300, num_users * 30)
         chart = alt.Chart(filtered_data).mark_bar().encode(
             y=alt.Y('Nome', sort=sort_order),
-            x=alt.X('Dias', scale=alt.Scale(round=True, domain=[0, filtered_data['Dias'].max()], nice=False), axis=alt.Axis(tickCount=filtered_data['Dias'].max())),  # Eixo X de 1 em 1
-            tooltip=['Nome', 'Dias']
+            x=alt.X('Total dias', scale=alt.Scale(round=True, domain=[0, filtered_data['Total dias'].max()], nice=False), axis=alt.Axis(tickCount=filtered_data['Total dias'].max())),
+            tooltip=['Nome', 'Total dias']
         ).properties(
             height=height_graph,
             width='container'
@@ -82,16 +78,16 @@ if uploaded_file is not None:
     
     else:   
         data_user = dataframe_clean[dataframe_clean['Nome'] == selected_user].copy()
-        data_user['Dia'] = data_user['Data'].dt.strftime('%d/%m/%Y')
+        data_user['Total dias'] = data_user['Data'].dt.strftime('%d/%m/%Y')
         
-        unique_days = data_user['Dia'].unique().tolist()
+        unique_days = data_user['Total dias'].unique().tolist()
         
         selected_day = st.selectbox(
             "Selecione o dia para ver os horários de acesso:",
             options=unique_days,
         )
         
-        access_day = data_user[data_user['Dia'] == selected_day]
+        access_day = data_user[data_user['Total dias'] == selected_day]
         
         st.write(f"Horários de acesso em {selected_day}:")
         st.dataframe(
@@ -100,5 +96,6 @@ if uploaded_file is not None:
             column_config={
                 "Data": st.column_config.DatetimeColumn("Horario de acesso", format="HH:mm:ss"),
                 "Sala": "Sala"
-            }
+            },
+            hide_index=True
         )
